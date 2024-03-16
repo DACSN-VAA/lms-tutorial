3e2a3d65198300975ac0df4c6387270aabd36ef0
Anh5the <lehoanganh2142003@gmail.com>
2024-03-17 01:37:41 +0700

    final

--- actions/get-dashboard-courses.ts
+++ actions/get-dashboard-courses.ts
@@ -44,6 +44,18 @@ export const getDashboardCourses = async (
       const progress = await getProgress(userId, course.id);
       course["progress"] = progress;
     }
+
+    const completedCourses = courses.filter(
+      (course) => course.progress === 100
+    );
+    const coursesInProgress = courses.filter(
+      (course) => (course.progress ?? 0) < 100
+    );
+
+    return {
+      completedCourses,
+      coursesInProgress,
+    };
   } catch (error) {
     console.log("[GET_DASHBOARD_COURSES]", error);
     return {
